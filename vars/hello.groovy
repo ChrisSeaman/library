3cfcd94fdd31363cf09b6178d8b3106ea34bdad1
Chris Seaman <chris.seaman@incontact.com>
2018-05-15 16:41:20 -0600

    Add options

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,7 +1,13 @@
 #!groovy
-def call() {
+def call(options) {
     // Have access to all the Jenkins plugins, etc
     sh 'echo Hello World'
+
+    // Capture the results of a shell script
+    def results = sh(script:"echo ${options}", returnStdout:true).trim()
+    // trim removes the return character...
+    echo "${results}"
+    // can now use if statements, etc now that we are in Groovy
 
     // these vars live only in groovy
     def groovyVar = "Something else"
